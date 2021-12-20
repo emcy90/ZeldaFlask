49c97a486c9512e4837cfd5d01fb5122fa0e622d
Emcy90 <emma@emcy.se>
2021-12-20 11:25:25 +0100

    adding structure

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/name', methods=['POST'])
+def get_name():
+    return "This is the name route"
 
 
 if __name__ == '__main__':
